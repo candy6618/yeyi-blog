d9046de245fe92d8970ff35401002cb6a45c4549
宋叶艺 <songyeyi@zj.tech>
2024-11-27 16:02:35 +0800

    feat: 修改信息

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/zh/', title: '主页' },
+  { href: '/zh/blog', title: '博客' },
+  { href: '/zh/tags', title: '标签' },
+  { href: '/zh/projects', title: '项目' },
+  { href: '/zh/about', title: '关于' },
 ]
 
 export default headerNavLinks
